8013ed48deaaa19b80e1a86ce2744b8f8ef27bd4
nexusboyko <alexboyko@outlook.com>
2025-02-16 15:35:10 -0800

    Update page routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import App from './App.tsx'
-import Room from './Room.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.tsx";
+import Room from "./Room.tsx";
+import "./index.css";
+import RoomsList from "./RoomsList.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<App />} />
-      
-        <Route path="/:id" element={<Room />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <GoogleOAuthProvider
+      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<App />} />
+          <Route path="/places" element={<RoomsList />} />
+
+          <Route path="/places/p/:id" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
